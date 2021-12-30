724437aeb54fdfb187e8e8d4824fef87bec62eaa
JohnB <john.allan.bracken@gmail.com>
2021-12-29 21:20:40 -0500

    commit

---
+++ File_type_count_summary.md
@@ -0,0 +1,19 @@
+# Summary of Number of files of Specific Extension From Home Directory
+
+* Number of .csv files found: 54
+* Number of .pdf files found: 431
+* Number of .jpg files found: 149
+* Number of .theme files found: 563
+* Number of .odt files found: 38
+* Number of .png files found: 7544
+* Number of .rb files found: 13
+* Number of .py files found: 4313
+* Number of .md files found: 127
+* Number of .txt files found: 425
+* Number of .xlsx files found: 7
+* Number of .R files found: 222
+* Number of .sas files found: 11
+* Number of .json files found: 1009
+* Number of .xml files found: 99
+* Number of .sql files found: 55
+* Number of .js files found: 551
---
+++ file_count.rb
@@ -0,0 +1,26 @@
+#Script to count the number of different types of files from my home
+#directory recursively.
+
+#Change directory to home
+Dir.chdir("/home/johnb")
+
+#List of file extensions to search
+file_extensions = ["csv", "pdf", "jpg", "theme", "odt", "png", 
+"rb", "py", "md", "txt", "xlsx", "R", "sas", "json",
+"xml", "sql", "js"]
+
+#Open file to write summary of file counts for each extension type.
+f = File.open(File.expand_path('/home/johnb/Documents/ruby/file_count/File_type_count_summary.md'), 'w')
+    f.puts "# Summary of Number of files of Specific Extension From Home Directory\n\n"
+
+    #Get list of files for each extension type and put count of them in a summary markdown file.
+    #Include the search into hidden files. 
+    file_extensions.each do |extension|
+    
+        subset_files = Dir["**/" + "*." + extension]
+        hidden_files = Dir["/home/johnb/\.[a-z]*/**/" + "*." + extension]
+        subset_files = subset_files.concat(hidden_files)
+        f.puts "* Number of " + "." +  extension + " files found: " + subset_files.length.to_s + "\n" 
+    end
+
+f.close
